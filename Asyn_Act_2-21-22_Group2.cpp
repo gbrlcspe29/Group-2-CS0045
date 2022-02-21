552f82fe57ce95ebd34daf2e6df2806f76bf5918
gbrlcspe29 <100104424+gbrlcspe29@users.noreply.github.com>
2022-02-21 12:20:29 +0800

    Add files via upload

---
+++ Asyn_Act_2-21-22_Group2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+//Caspe
+void Render1();
+void display();
+void points();
