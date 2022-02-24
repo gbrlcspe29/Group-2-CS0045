eed936dc8497a26f7e6bd9d2a705ecaeacf219c0
mrynlws <80335563+mrynlws@users.noreply.github.com>
2022-02-24 10:31:33 +0800

    Add files via upload

---
+++ TriangleVertex_02-24-2022_Group2.cpp
@@ -0,0 +1,72 @@
+//Caspe
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void triangle();
+void points();
+void display();
+
+//Cordero
+int main(int argc, char **argv){
+	glutInit(&argc, argv);
+	glutInitWindowSize(700,700);
+	glutInitWindowPosition(200,50);
+	glutCreateWindow("My First OpenGL");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
+
+//Cutaran
+void triangle(){
+	GLfloat triangleVertex[] = {
+		//x	    y      z
+		0.0f, 0.75f, 0.0f,
+	    -0.65f, 0.30f, 0.0f,
+		0.65f, 0.30f, 0.0f,
+		
+		-0.65f, -0.15f, 0.0f,
+	    0.0f, 0.30f, 0.0f,
+		0.65f, -0.15f, 0.0f,
+		
+		-0.65f, -0.60f, 0.0f,
+	    0.0f, -0.15f, 0.0f,
+		0.65f, -0.60f, 0.0f
+	};
+    
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, triangleVertex);
+	glDrawArrays(GL_TRIANGLES, 0, 9);
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+//Buama
+void points(){
+	glPointSize(20.0);
+	
+	GLfloat pointVertex[] = {
+	-0.75f, -0.75f, 0.0f,
+	-0.75f, 0.75f, 0.0f,
+	0.75f, 0.75f, 0.0f,
+	0.75f, -0.75f, 0.0f
+	};
+	
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, pointVertex);
+	glDrawArrays(GL_POINTS, 0, 4);
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+//Bernardino
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(2.0f, 0.5f, 1.0f); //Lilac
+	triangle();
+	points();
+	glFlush();
+}
